f065dd2813201e2448c44a5f6436f38f73fd8e21
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-21 21:08:39 +0100

    add auth user routes

--- server.js
+++ server.js
@@ -12,11 +12,88 @@ app.use(bodyParser.json());
 /image -> PUT => user 
 */
 
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'gizmo',
+      email: 'gizmo@email.com',
+      password: 'treats',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'rufus',
+      email: 'rufus@email.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
+// GET registered users
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.send(database.users);
 });
 
+// POST user auth credentials
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('🐶 success');
+  } else {
+    res.status(400).json('💩 error logging in');
+  }
+});
+
+// POST register a new user
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+// GET user profile
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('💩 not found');
+  }
+});
+
+// PUT a new user image entry
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json('💩 not found');
+  }
+});
 
 // Initialize the app.
 const server = app.listen(process.env.PORT || 3000, function () {

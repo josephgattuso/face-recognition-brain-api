7afbc74bcab06e89f70a6d50cc2829c313566b0a
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-22 10:38:28 +0100

    add cors package

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 /*
 / -> res = this is working

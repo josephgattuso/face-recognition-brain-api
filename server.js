ef1f64f92203cf6196aa7e71528ad5c8f582d8ba
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-25 19:01:00 +0100

    update db

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'joeetuso',
-    password: '',
-    database: 'smart-brain',
+    connectionString: process.env.DATABASE_URL,
   },
 });
 
